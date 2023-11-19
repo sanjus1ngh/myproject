75b0cb7b6c93d9219423d34d8245ec8f8acac65a
sanju_s1ngh <sanjuvaishnavisingh7033@gmail.com>
2023-11-19 10:20:43 +0530

    new added feature

---
+++ myapp/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from myapp.models import Contact
+from django.contrib import messages
+
+# Create your views here.
+def index(request):
+    context = {
+        "variable":"this is sent"
+    }
+    
+    return render(request, 'index.html', context)
+    #return HttpResponse("this is homepage")
+
+def about(request):
+    #return HttpResponse("this is about page")
+    return render(request, 'about.html')
+
+def services(request):
+    #return HttpResponse("this is service page")
+    return render(request, 'services.html')
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc, date= datetime.today())
+        contact.save()
+        messages.success(request,"Your Message has been.")
+        
+    #return HttpResponse("this is contact page")
+    return render(request, 'contact.html')
